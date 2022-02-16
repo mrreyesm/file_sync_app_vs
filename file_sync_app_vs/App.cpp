a2373e64663f7d4a80482480a70b9ffcc072f4c1
Tilo Wendler <wendler@htw-berlin.de>
2022-02-16 19:26:22 +0100

    Test2

--- file_sync_app_vs/App.cpp
+++ file_sync_app_vs/App.cpp
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        File Sync
 // Purpose:     An application that Syncs Files
-// Developed:   15.01.2022 @HTW Berlin under the Coordination of Tilo Wendler
+// Developed:   15.01.2022 @HTW Berlin under the Coordination of T. Wendler
 /////////////////////////////////////////////////////////////////////////////
 #include "App.h"
 #include "id.h"
